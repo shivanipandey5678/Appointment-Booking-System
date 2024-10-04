0f14ac70cdbb74aaf16c24447fef60079c6a7764
Shivani Pandey <shivanipandey0107@gmail.com>
2024-10-04 21:00:00 +0530

    style: finalized responsive Navbar UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,12 +29,19 @@ const Navbar = () => {
             </NavLink>
         </ul>
         
-            <div className='flex-item-center gap-4 group-relative'>
+            <div className='flex-item-center gap-4 relative'>
             {
             token?
-            <div className='flex items-center gap-2 cursor-pointer'>
+            <div className='flex items-center gap-2 cursor-pointer relative group'>
                 <img src={assets.profile_pic} alt="profile_pic" className="rounded-full w-8" />
                 <img src={assets.dropdown_icon} alt="dropdown_icon" className='w-2.5 '/>
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-grey-600 z-20 hidden group-hover:block'>
+                    <div className='min-w-48 bg-stone-100 rounded flex flex-col p-4 gap-4 '>
+                        <p onClick={()=>navigate("/my-profile")} className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>navigate("/my-appointment")} className='hover:text-black cursor-pointer'>My Appointments</p>
+                        <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                    </div>
+                </div>
             </div>:
             <button  onClick={()=>navigate("/login")} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>
                 Create account
