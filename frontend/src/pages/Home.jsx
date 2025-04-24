de9cd9973fdc2d2125854831cde9d9ab3e405634
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2025-04-24 14:37:44 +0530

    Correcting Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
-import SpecialityMenu from '../components/specialityMenu'
+import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
